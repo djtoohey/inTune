712f43e4657c805f5c451f20dff223ba50a9119a
djtoohey <djtoohey@gmail.com>
2020-10-24 04:30:36 +1030

    production

---
+++ client/build/precache-manifest.4ded550fafdf35783ecf8614889b5476.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452f1e30c9056ebe286af9acf14179a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbf7975b101719b35a71",
+    "url": "/static/css/main.7cd9d06f.chunk.css"
+  },
+  {
+    "revision": "a5177b3f64fa0bf531e8",
+    "url": "/static/js/2.a27bb8f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a27bb8f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf7975b101719b35a71",
+    "url": "/static/js/main.f20bed5c.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  }
+]);
