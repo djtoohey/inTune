dc2e1d78f7ba582db1a1508b1f093ab7687504e6
djtoohey <djtoohey@gmail.com>
2020-10-28 23:59:47 +1030

    fix state

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     }
 
     render() {
-        const { token } = this.state;
+        const { token } = this.state.token;
         return (
 
             <div>
--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Btn from "../../components/Btn";
 
 
 function Login() {
