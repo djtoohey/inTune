63e7314bf574e1a249009950eef3c20bf2eb9ea2
djtoohey <djtoohey@gmail.com>
2020-10-29 00:20:01 +1030

    still trying

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -3,7 +3,13 @@ import React from "react";
 
 function Login() {
     return (
-        <a href="https://djtoohey-intune.herokuapp.com/login" ><button >Login</button></a>
+        <div className="hero is-medium is-fluid">
+            <div className="hero-body container has-text-centered ">
+                <h1 className="title has-text-white">inTune</h1>
+                <h2 className="subtitle has-text-white">for Spotify</h2>
+                <a href="https://djtoohey-intune.herokuapp.com/login" >Login</a>
+            </div>
+        </div>
     )
 }
 
